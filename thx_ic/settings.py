c975d5e6bd22c39ba8cc4d11b9feadfa91a6eba4
Iulian Matei <iulianmatei19768@gmail.com>
2023-07-18 08:27:46 +0300

    update1

--- thx_ic/settings.py
+++ thx_ic/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-v%y%@ar8tu1@1)6$vy&)ow=-tgfnf)36s65hv@t(s-!fzmt=w@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

3c0c68d7fe48b3105b0b2736cb549dcf103766c1
Iulian Matei <iulianmatei19768@gmail.com>
2023-07-18 09:08:02 +0300

    update 8

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
 from . import views
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 urlpatterns = [
     
     path('', views.home, name='gradi-acasa'),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
